098c023a6674a636e252a054f1a72abdaa2a4a4d
alomina007 <alomina007@users.noreply.github.com>
2016-06-19 10:45:32 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,10 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "supergroup",
+    "supergroupfa",
+    "info"
     },
     sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
